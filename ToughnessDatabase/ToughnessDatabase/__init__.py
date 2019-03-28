948cd96f63ad25775168620ec6f4d77f8681cf6f
Ravinder Singh <ravi_29_12@hotmail.com>
2019-03-28 23:04:49 +0000

    Added code for CSRF token.

--- ToughnessDatabase/ToughnessDatabase/__init__.py
+++ ToughnessDatabase/ToughnessDatabase/__init__.py
@@ -3,6 +3,10 @@ The flask application package.
 """
 
 from flask import Flask
+from flask_wtf.csrf import CsrfProtect
+
+csrf = CsrfProtect()
 app = Flask(__name__)
+csrf.init_app(app)
 
 import ToughnessDatabase.views
