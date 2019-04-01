d7ac928dc5b33fed1c6538625906386c929be6b1
Ravinder Singh <ravi_29_12@hotmail.com>
2019-04-01 23:04:05 +0100

    Working structure.

--- ToughnessDatabase/ToughnessDatabase/forms.py
+++ ToughnessDatabase/ToughnessDatabase/forms.py
@@ -9,51 +9,98 @@ from wtforms.fields import StringField, \
         TextAreaField, SelectField, SubmitField, \
         FormField, FieldList
 from wtforms.validators import DataRequired, \
-        InputRequired, Optional, NumberRange
+        InputRequired, Optional, NumberRange     
+
+
+class PipelineNameListForm(FlaskForm):
+    class Meta:
+        csrf = False
+    
+    short_name = StringField("Line Short Name", validators=[DataRequired()])
+    name = StringField("Name")
+
 
 class ProjectForm(FlaskForm):
     title = "Project Details"
     name = StringField("Project Name", validators=[DataRequired()])
     prj_no = StringField("Project Number", validators=[DataRequired()])
     region_prj_no = StringField("Regional Project Number", validators=[DataRequired()])
-
-    owner = StringField("Owner(s)")
-    operator = StringField("Operator(s)")
-    
+    client = StringField("Client(s) Names")
+    prj_type = StringField("Type of project,assessment, etc.")
     country = StringField("Country")
     state = StringField("State")
-    city = StringField("City")    
+    city = StringField("City")
+    pipelines = FieldList(FormField(PipelineNameListForm), min_entries=1)
     go_next = SubmitField("Next")
-            
+
 
 class PipelineForm(FlaskForm):
     title = "Pipeline Information"
-    name = StringField("Pipeline Name")
-    short_name = StringField("Line Name")
-    length = DecimalField("Pipeline\"s Length")
-    length_units = RadioField("Length Units", choices=[("m", "m"), ("ft", "ft")])
-
-    design_code = FieldList(StringField("Design Code"))
-    year_constructed = FieldList(DecimalField("Construction / Commissioning Year"))
-
-    diameter = FieldList(DecimalField("Outer Diameter"))
-    diameter_units = RadioField("Diameter Units", choices=[("mm", "mm"), ("inch", "inch")])
-
-    thickness = FieldList(DecimalField("Wall Thickness"))
-    thickness_units = RadioField("Thickness Units", choices=[("mm", "mm"), ("inch", "inch")])
     
-    grade = FieldList(SelectField("Material Grade"))
-    seam_Weld_type = FieldList(SelectField("Weld Type"))
-    psl_no = FieldList(SelectField("Product Specification Level (PSL)", choices=[("PSL1", "PSL 1"), ("PSL2", "PSL 2")]))
-    manufacturer = FieldList(StringField("Pipe Manufacturer"))
+    diameter = FieldList(DecimalField("Outer Diameter"), min_entries=1, validators=[Optional()])
+    diameter_units = RadioField("Diameter Units", choices=[("mm", "mm"), ("inch", "inch")], validators=[Optional()])
+    thickness = FieldList(DecimalField("Wall Thickness"), min_entries=1, validators=[Optional()])
+    thickness_units = RadioField("Thickness Units", choices=[("mm", "mm"), ("inch", "inch")], validators=[Optional()])
+    length = DecimalField("Pipeline Length", validators=[Optional()])
+    length_units = RadioField("Length Units", choices=[("m", "m"), ("ft", "ft")], validators=[Optional()])
+    
+    grade = FieldList(SelectField("Material Grade", choices=[("",""), ("b", "Grade B"), ("X42", "Grade X42"), ("X52", "Grade X52")]), min_entries=1)
+    seam_weld_type = FieldList(SelectField("Weld Type", choices=[("",""), ("DSAW", "DSAW"), ("EFW", "EFW"), ("ERW", "ERW"), ("SMLS", "SMLS")]), validators=[Optional()], min_entries=1)
+    psl_no = FieldList(SelectField("Product Specification Level (PSL)", choices=[("",""), ("PSL1", "PSL 1"), ("PSL2", "PSL 2")]), validators=[Optional()], max_entries=2, min_entries=1)
+    manufacturer = FieldList(StringField("ManufacturerName"), min_entries=1, validators=[Optional()])
+    year_constructed = FieldList(DecimalField("Construction Year"), validators=[Optional()], min_entries=1)
+    
+    design_code = FieldList(StringField("Design Code"), min_entries=1)
+    design_press = FieldList(DecimalField("Design Pressure"), validators=[Optional()], min_entries=1)
+    design_press_units = RadioField("Pressure Units", choices=[("bar", "bar"), ("psi", "psi")], validators=[Optional()])
+    design_temp = FieldList(DecimalField("Design Temperatures"), validators=[Optional()], min_entries=1)
+    design_temp_units = RadioField("Temperature Units", choices=[("C", "°C"), ("F", "°F")], validators=[Optional()])
+    design_life = DecimalField("Design Life in Years", validators=[Optional()])
+    year_commissioned = DecimalField("Commissioning Year", validators=[Optional()])
 
-    presure = FieldList(DecimalField("Design / Operating Pressure"))
-    press_units = RadioField("Pressure Units", choices=[("bar", "bar"), ("psi", "psi")])
+    oper_press = FieldList(DecimalField("Operating Pressure"), validators=[Optional()], min_entries=1)
+    oper_press_units = RadioField("Pressure Units", choices=[("bar", "bar"), ("psi", "psi")], validators=[Optional()])
+    oper_temp = FieldList(DecimalField("Operating Temperatures"), validators=[Optional()], min_entries=1)
+    oper_temp_units = RadioField("Temperature Units", choices=[("C", "°C"), ("F", "°F")], validators=[Optional()])
 
-    test_pres = FieldList(DecimalField("Test Pressure"))
-    test_press_units = RadioField("Test Pressure Units", choices=[("bar", "bar"), ("psi", "psi")])
-
-    temp = FieldList(DecimalField("Design / Operating Temperatures"))
-    temp_units = RadioField("Temperature Units", choices=[("C", "°C"), ("F", "°F")])
-
+    test_press = FieldList(DecimalField("Test Pressure"), validators=[Optional()], min_entries=1)
+    test_press_units = RadioField("Test Pressure Units", choices=[("bar", "bar"), ("psi", "psi")], validators=[Optional()])
+    test_year = DecimalField("Test Year", validators=[Optional()])
+    
+    go_back = SubmitField("Back")
     go_next = SubmitField("Next")
+
+class TensileTestForm(FlaskForm):
+    title = "Tensile Test Data"
+    test_spool = SelectField("Tensile Test Spool", validators=[Optional()])
+    test_code = FieldList(StringField("Testing Standard"), min_entries=1)
+    yields = FieldList(DecimalField("Test Yield Strength"), min_entries=1, validators=[Optional()])
+    yield_units = RadioField("Pressure Units", choices=[("MPa", "MPa"), ("ksi", "ksi")], validators=[Optional()])
+    tensile = FieldList(DecimalField("Test Tensile Strength"), min_entries=1, validators=[Optional()])
+    tensile_units = RadioField("Pressure Units", choices=[("MPa", "MPa"), ("ksi", "ksi")], validators=[Optional()])
+    
+    modulus = FieldList(DecimalField("Elastic Modulus"), min_entries=1, validators=[Optional()])
+    modulus_units = RadioField("Elastic Modulus Units", choices=[("MPa", "MPa"), ("ksi", "ksi")], validators=[Optional()])
+    poisson = FieldList(DecimalField("Poission\'s Ratio"), min_entries=1, validators=[Optional()])
+    therm_coeff = FieldList(DecimalField("Coefficient of Thermal Expansion"), min_entries=1, validators=[Optional()])
+    therm_coeff_units = RadioField("Test Pressure Units", choices=[("umc", "μm/m °C"), ("uif", "μin/in °F")], validators=[Optional()])
+    # TODO: stress-strain curve
+    go_back = SubmitField("Back")
+    go_next = SubmitField("Next")
+
+class CharpyForm(FlaskForm):
+    class Meta:
+        csrf = False
+    size = SelectField("Specimen Size", choices=[("full","Full"), ("half", "Half")], validators=[DataRequired()])
+    charpy = DecimalField("Charpy Energy", validators=[DataRequired()])
+    location = SelectField("Test Location", choices=[("WM", "WM"), ("HAZ", "HAZ"), ("BM", "BM"), ("FL", "FL"), ("FL2", "FL+2mm"), ("FL5", "FL+5mm")], validators=[DataRequired()])
+    shear_area = DecimalField("Shear Area (%)", validators=[Optional()])
+    temperature = DecimalField("Test Temperature", validators=[Optional()])
+    
+class CharpyTestForm(FlaskForm):
+    title = "Charpy Test Data"
+    test_code = FieldList(StringField("Testing Standard"), min_entries=1)
+    chapry_data = FieldList(FormField(CharpyForm), min_entries=1)
+    charpy_units = RadioField("Charpy Energy Units", choices=[("J", "J"), ("ftlbs", "ft-lbs")], validators=[DataRequired()])
+    temp_units = RadioField("Temperature Units", choices=[("C", "°C"), ("F", "°F")], validators=[Optional()])
+
--- ToughnessDatabase/ToughnessDatabase/views.py
+++ ToughnessDatabase/ToughnessDatabase/views.py
@@ -3,62 +3,213 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template, session, request
+from flask import render_template, session, \
+                  request, jsonify, url_for, \
+                  redirect
 from ToughnessDatabase import app, forms
 from os import environ
+from functools import wraps
+import itertools
+global str
 
-@app.route('/')
-@app.route('/home')
+def project_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if "project" in session:
+            return f(*args, **kwargs)
+        else:
+            # flash("You need to login first")
+            return "Project and pipeline details are required"
+
+    return wrap
+
+@app.route("/")
+@app.route("/home")
 def home():
     """Renders the home page."""
     return render_template(
-        'index.html',
-        title='Home Page',
+        "index.html",
+        title="Home Page",
         year=datetime.now().year,
         username=environ["USERNAME"]
     )
 
-@app.route('/contact')
+@app.route("/contact")
 def contact():
     """Renders the contact page."""
     return render_template(
-        'contact.html',
-        title='Contact',
+        "contact.html",
+        title="Contact",
         year=datetime.now().year,
         username=environ["USERNAME"],
-        message='Your contact page.'
+        message="Your contact page."
     )
 
-@app.route('/about')
+
+@app.route("/ext/about")
 def about():
     """Renders the about page."""
     return render_template(
-        'about.html',
-        title='About',
+        "about.html",
+        title="About",
         year=datetime.now().year,
         username=environ["USERNAME"],
-        message='Your application description page.'
+        message="Your application description page."
     )
 
-@app.route('/add_to_db/<page_name>', methods=['GET', 'POST', 'PUT'])
-def add_to_db(page_name):
+
+@app.route("/add", methods=["GET", "POST", "PUT", "DELETE"])
+def add():
     """Renders the add test database page layout."""
-
-
-
-
-    default_page_name = 'default.html'
-    form = {'title': "Add Test Data to Database"}
-
-    if page_name == 'projectdetails.html':
-        form = forms.ProjectForm()
-    if page_name == "pipelineinfo.html":
-        form = forms.PipelineForm()      
-        
+    form = {"title": "Add Test Data"}
     return render_template(
-        'forms/{}'.format(page_name),
-        title = 'Add Test Data',
+        "add.html",
+        title = "Add Test Data",
         year = datetime.now().year,
         username=environ["USERNAME"],
         form = form
-    )
+        )
+
+
+@app.route("/add/project", methods=["GET", "POST"])
+def add_project():
+    """Renders the add test database page layout."""
+
+    try:
+        prj = session["project"]
+        form = forms.ProjectForm(request.form, data=prj)
+    except:
+        form = forms.ProjectForm(request.form)
+    
+    print(form.go_next.data)
+    if request.method == "POST" and form.validate_on_submit():
+        session["project"] = {
+            "name": form.name.data,
+            "prj_no": form.prj_no.data,
+            "region_prj_no": form.region_prj_no.data,
+            "client": form.client.data,
+            "prj_type": form.prj_type.data,
+            "country": form.country.data,
+            "state": form.state.data,
+            "city": form.city.data,
+            "pipelines": [data for data in form.pipelines.data]            
+        }
+        session["pipelines"] = {}    
+        return redirect(url_for("add_pipeline"))
+
+    return render_template(
+        "forms/project.html",
+        title = "Add Test Data",
+        year = datetime.now().year,
+        username = environ["USERNAME"],
+        form = form
+        )
+
+@app.route("/add/pipeline")
+def add_pipeline():
+    """Renders the available pipelines."""
+
+    form = {"title": "Pipelines"}
+    return render_template(
+        "forms/pipeline.html",
+        title = "Add Pipeline Information",
+        year = datetime.now().year,
+        username = environ["USERNAME"],
+        pipelines = session["project"]["pipelines"],
+        form = form
+        )
+
+
+@app.route("/add/pipeline/<string:line_name>", methods=["GET", "POST"])
+def add_pipeline_info(line_name):
+    """Renders the add test database page layout."""
+
+    try:
+        prj = session["pipelines"][line_name]
+        form = forms.PipelineForm(request.form, data=prj)
+    except:
+        form = forms.PipelineForm(request.form)
+    
+    form.title += " - {}".format(line_name)
+
+    if request.method == "POST" and form.validate_on_submit():
+        session["pipelines"] = {
+            line_name: {
+                "diameter": [str(dia) for dia in form.diameter.data],
+                "diameter_units": form.diameter_units.data,
+                "thickness": [str(wt) for wt in form.thickness.data],
+                "thickness_units": form.thickness_units.data,
+                "length": str(form.length.data),
+                "length_units": form.length_units.data,
+                
+                "grade": [gr for gr in form.grade.data],
+                "seam_weld_type": [swt for swt in form.seam_weld_type.data],
+                "psl_no": [psl for psl in form.psl_no.data],
+                "manufacturer": [man for man in form.manufacturer.data],
+                "year_constructed": [yc for yc in form.year_constructed.data],
+                
+                "design_code": [dc for dc in form.design_code.data],
+                "design_press": [str(dp) for dp in form.design_press.data],
+                "design_press_units": form.design_press_units.data,
+                "design_temp": [str(dt) for dt in form.design_temp.data],
+                "design_temp_units": form.design_temp_units.data,
+                "design_life": str(form.design_life.data),
+                "year_commissioned": str(form.year_commissioned.data),
+
+                "oper_press": [str(op) for op in form.oper_press.data],
+                "oper_press_units": form.oper_press_units.data,
+                "oper_temp": [str(ot) for ot in form.oper_temp.data],
+                "oper_temp_units": form.oper_temp_units.data,
+
+                "test_press": [str(tp) for tp in form.test_press.data],
+                "test_press_units": form.test_press_units.data,
+                "test_year": str(form.test_year.data)
+            }
+        }
+        
+        if (form.go_back.data):
+            return (redirect(url_for("add_pipeline")))
+        
+        #if form.go_next.data and len(session["pipelines"]) == len(session["project"]["pipelines"]):
+            #return redirect(url_for('add'))
+        #    print("doing")
+        #else:
+            # TODO: code this up
+        #    print("redirecting")
+            ##return redirect(url_for('add_pipeline_info', ))
+        
+    return render_template(
+        "forms/pipelineinfo.html",
+        title = "Pipeline Data",
+        year = datetime.now().year,
+        username = environ["USERNAME"],
+        line_name=line_name,
+        form = form
+        )
+
+
+@app.route("/add/tensile-test/<string:line_name>", methods=["GET", "POST"])
+# @project_required
+def add_tensile_test(line_name):
+    """Renders the add test database page layout."""
+
+    try:
+        prj = session["tensile_tests"][line_name]
+        form = forms.TensileTestForm(request.form, data=prj)
+    except:
+        form = forms.TensileTestForm(request.form)
+    
+    form.title += " - {}".format(line_name)
+
+    if request.method == "POST" and form.validate_on_submit():
+        print("POST")
+        
+        
+    return render_template(
+        "forms/tensileinfo.html",
+        title = "Tensile Test Data",
+        year = datetime.now().year,
+        username = environ["USERNAME"],
+        line_name=line_name,
+        form = form
+        )
